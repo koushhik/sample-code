f68de1f0a03e71f8f550fac2f9c9a87dd64092fc
KOUSHIK <54579643+koushhik@users.noreply.github.com>
2019-11-30 16:42:37 +0530

    Create Fc.c

---
+++ Fc.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#define MAX_SCORE 1001
+ 
+int bullet[MAX_SCORE];
+int coder[MAX_SCORE];
+int prev[MAX_SCORE];
+ 
+int main() 
+{
+ int N;
+printf("enter the number of values\n");
+scanf("%d",&N);
+int tmp;
+
+for(int i = 0; i < N; i++)
+{
+   printf("enter the tmp\n");
+   scanf("%d", &tmp);
+   bullet[tmp]++;
+   coder[tmp]++;
+}
+
+int current;
+tmp = -1;
+for(int i = 1; i <MAX_SCORE ; i++)
+{
+   if(coder[i] != 0)
+   {
+       prev[i] = tmp;
+       current = tmp;
+       tmp = i;
+       
+       while(current != -1 && bullet[i] != 0)
+       {
+           
+           if(bullet[i] >= coder[current])
+           {
+               bullet[i] -= coder[current];
+               coder[current] = 0;
+               prev[i] = prev[current];
+           }
+           else
+           {
+               coder[current] -= bullet[i];
+               bullet[i] = 0;
+           }
+           
+           current = prev[current];
+       }
+       
+   }
+}
+int sum;
+
+current = tmp;
+while(current != -1)
+{
+   sum += current * coder[current];
+   current = prev[current];
+}
+
+
+printf("%d",sum);
+
+return 0;
+ 
+}
